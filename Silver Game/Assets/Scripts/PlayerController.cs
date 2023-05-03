8b49a9a5be011e3cd49e747a7463f4f9b135dd53
xSawl <132205684+xSawl@users.noreply.github.com>
2023-05-03 20:49:49 +0200

    Editing Player Controller

--- Silver Game/Assets/Scripts/PlayerController.cs
+++ Silver Game/Assets/Scripts/PlayerController.cs
@@ -5,7 +5,12 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D rb;
-    private FilterMode movementInputDirection;
+
+    private float movementInputDirection;
+    private bool isFacingRight = true;
+
+    public float movementSpeed = 10f;
+    public float jumpForce = 4f;
 
     void Start()
     {
@@ -14,6 +19,45 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        
+        checkInput();
+        CheckMovementDirection();
+    }
+
+    private void FixedUpdate() {
+        applyMovement();
+    }
+
+    private void checkInput() 
+    {
+        movementInputDirection = Input.GetAxisRaw("Horizontal");
+
+        if(Input.GetButtonDown("Jump")){
+            Jump();
+        }
+    }
+
+    private void applyMovement()
+    {
+        rb.velocity = new Vector2(movementSpeed * movementInputDirection, rb.velocity.y);
+    }
+
+    private void CheckMovementDirection(){
+        if(isFacingRight && movementInputDirection < 0)
+        {
+            Flip();
+        }
+        else if(!isFacingRight && movementInputDirection > 0){
+            Flip();
+        }
+
+    }
+
+    private void Flip(){
+        isFacingRight = !isFacingRight;
+        transform.Rotate(0.0f, 180.0f, 0.0f);
+    }
+
+    private void Jump(){
+        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
     }
 }
